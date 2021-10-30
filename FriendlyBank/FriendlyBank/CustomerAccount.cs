76201a269af68fd7678c8be4a8d03fc2c7b53100
Wahyu <wahjoe.eko.n@gmail.com>
2021-10-30 13:51:59 +0700

    [mod] add Save and Load CustomerAccount
    - and fix the constructor

--- FriendlyBank/FriendlyBank/CustomerAccount.cs
+++ FriendlyBank/FriendlyBank/CustomerAccount.cs
@@ -8,12 +8,56 @@ namespace FriendlyBank
 {
 	public class CustomerAccount : Account
 	{
-		public CustomerAccount(string inName, int inBalance) : 
+		public CustomerAccount(string inName, decimal inBalance) : 
 			base (inName, inBalance){}
 
 		public override string RudeLetterString()
 		{
 			return "You are overdrawn";
+		}
+
+		public bool Save(string filename)
+		{
+			try
+			{
+				System.IO.TextWriter textOut =
+					new System.IO.StreamWriter(filename);
+
+				textOut.WriteLine(GetName());
+				textOut.WriteLine(GetBalance());
+				textOut.Close();
+			}
+			catch
+			{
+				return false;
+			}
+
+			return false;
+		}
+
+		public static CustomerAccount Load(string filename)
+		{
+			CustomerAccount result = null;
+			System.IO.TextReader textIn = null;
+
+			try
+			{
+				textIn = new System.IO.StreamReader(filename);
+				string nameText = textIn.ReadLine();
+				string balanceText = textIn.ReadLine();
+				decimal balance = decimal.Parse(balanceText);
+				result = new CustomerAccount(nameText, balance);
+			}
+			catch
+			{
+				return null;
+			}
+			finally
+			{
+				if (textIn != null) textIn.Close();
+			}
+
+			return result;
 		}
 	}
 }
