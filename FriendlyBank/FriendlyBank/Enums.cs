eac502c00104c23e60493a0e1bd289d3f8c5d39e
Wahyu <wahjoe.eko.n@gmail.com>
2021-10-30 13:24:17 +0700

    [add] public enum

---
+++ FriendlyBank/FriendlyBank/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendlyBank
+{
+	public enum AccountState
+	{
+		New,
+		Active,
+		UnderAudit,
+		Frozen,
+		Closed
+	}
+}
